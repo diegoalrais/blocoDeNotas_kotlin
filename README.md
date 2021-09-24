bef4753125e7da3e7bb358acf99d81888c4f4cd3
DIEGO MORAIS <diego_alrais@hotmail.com>
2021-09-24 10:27:31 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# App Bloco de Notas
+
+#### Atividade do curso Desenvolvimento Android com Kotlin na Udemy, ministrado pelo Profº Marcos Duarte Gomes.
+
+##### Minhas alterações:
+
+1. Mudei a colorPrimary;
+2. Coloquei uma imagem antes do EditText.
+
+
+
+[Apresentação](https://youtu.be/K3fQZ5avW8c)
+
+
+
+[![Linkedin Badge](https://img.shields.io/badge/-Diego_Morais-blue?style=flat-square&logo=Linkedin&logoColor=white&link=https://www.linkedin.com/in/diego-morais-9666a61a9)](https://www.linkedin.com/in/diego-morais-9666a61a9)  [![Gmail Badge](https://img.shields.io/badge/-diegoalrais@gmail.com-c14438?style=flat-square&logo=Gmail&logoColor=white&link=mailto:diegoalrais@gmail.com)](mailto:diegoalrais@gmail.com)
+
---
+++ app/src/main/java/com/diegoalrais/blocodenotas/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.diegoalrais.blocodenotas
+
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+import com.diegoalrais.blocodenotas.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val preferencia = PreferenciaAnotacao(applicationContext)
+        val botaoSalvar = binding.fab
+
+        botaoSalvar.setOnClickListener {
+            val anotacaoRecuperada = binding.editContainer.editAnotacao.text.toString()
+
+            if (anotacaoRecuperada == "") {
+                Toast.makeText(this, "Digite alguma coisa...", Toast.LENGTH_SHORT).show()
+            } else {
+                preferencia.SalvarAnotacao(anotacaoRecuperada)
+                Toast.makeText(this, "Anotação salva com sucesso! ", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+            val anotacao = preferencia.RecuperarAnotacao()
+            if (anotacao != "") {
+                binding.editContainer.editAnotacao.setText(anotacao)
+            }
+
+
+
+
+    }
+}
